cacb19f625680dc1284bc5b1bcc6745832dfe145
zhangwei66 <zhangwei66@xiaomi.com>
2021-09-02 16:59:55 +0800

    feat: 缓存

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import Print from './print';
 
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
-  return element;
+  element.innerHTML = _.join(['Hello', 'webpssack'], '');
+  element.onclick = Print.bind(null, 'Hello webpack!');
+  return element
 }
 
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,35 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
+const webpack = require('webpack')
 
 module.exports = {
   mode: "production",
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
   },
   output: {
-    filename: '[name].bundle.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
     clean: true, // 清除旧文件
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: '管理输出',
+      title: 'Caching',
     }),
-    new WebpackManifestPlugin()
+    new WebpackManifestPlugin(),
   ],
+  optimization: {
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
 };
