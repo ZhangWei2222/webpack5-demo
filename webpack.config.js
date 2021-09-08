7708bcf6e0252c4644aa0a53448e197f21baf731
zhangwei2222 <985983437@qq.com>
2021-09-08 22:16:31 +0800

    feat: shimming 预置依赖

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,25 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
   mode: "production",
-  entry: './src/index.js',
+  entry: {
+    polyfills: './src/polyfills',
+    index: './src/index.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      join: ['lodash', 'join'],
+    }),
+  ],
+  module: {
+    // rules: [{
+    //   test: require.resolve('./src/globals.js'), // 有报错
+    //   use: 'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse',
+    // }, ]
+  }
 };
